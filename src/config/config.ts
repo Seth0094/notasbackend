c531eb8399254bc70e622622e2f7003ed94b0d2b
Seth <98616042+Seth0094@users.noreply.github.com>
2024-04-03 00:38:20 -0400

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 export default {
   jwtSecret: process.env.JWT_SECRET || 'tokensecreto',
   DB: {
-    URI: process.env.MONGODB_URI || 'mongodb+srv://rodolfo:musubi88@cluster0.fvapyht.mongodb.net/?retryWrites=true&w=majority',
+    URI: process.env.MONGODB_URI || '',
     USER: process.env.MONGODB_USER,
     PASSWORD: process.env.MONGODB_PASSWORD
   }
